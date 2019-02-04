d671fccbe8ff123c8adbaabebef2bc9edeeae444
DESKTOP-Q3BTUMG\royst <Roy@delcom.nl>
2019-02-04 12:02:06 +0100

    Consistency in using _context not _tmc

--- TimeManagementTool/Controllers/ProcessController.cs
+++ TimeManagementTool/Controllers/ProcessController.cs
@@ -12,11 +12,11 @@ namespace TimeManagementTool.Controllers
     class ProcessController
     {
         private MainWindow _view;
-        private TimeManagementContext _tmc;
+        private TimeManagementContext _context;
         public ProcessController(MainWindow view)
         {
             _view = view;
-            _tmc = new TimeManagementContext();
+            _context = new TimeManagementContext();
         }
 
         public void GetRunningProcesses()
@@ -28,17 +28,17 @@ namespace TimeManagementTool.Controllers
 
         public void GetProccessesByCategory(Category category)
         {
-            List<ProcessCategory> categoryProcesses = _tmc.Processes.Where(cp => cp.Category.Id == category.Id).ToList();
+            List<ProcessCategory> categoryProcesses = _context.Processes.Where(cp => cp.Category.Id == category.Id).ToList();
             _view.ShowProcessesByCategory(categoryProcesses);
 
         }
 
         public void AddProcessToCategory(int categoryId, int processId, string processName)
         {
-                Category c = _tmc.Categories.Find(categoryId);
+                Category c = _context.Categories.Find(categoryId);
                 ProcessCategory processCategory = new ProcessCategory(processId, processName, c);
-                _tmc.Processes.Add(processCategory);
-                _tmc.SaveChanges();
+                _context.Processes.Add(processCategory);
+                _context.SaveChanges();
                 _view.AddSingleProcessToProcessByCategoryList(processCategory);
         }
     }
